ba21b68ac94374b0fa2143285376878569d2790a
631593796 <631593796@qq.com>
2020-11-24 12:52:16 +0800

    主体头部内容未完成

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).on('mousewheel',()=>{
+     var i= $(document).scrollTop();
+     if(i<=175){
+      $('.m-top-xd-box').css('display',"none")
+     }
+     if(i >= 175){
+      $('.m-top-xd-box').css('display',"block")
+     }
+})
